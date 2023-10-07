76d4e7aef33069889a1a381313c60682905f7b91
ArJSarmiento <rneljan@gmail.com>
2023-10-07 23:55:09 +0800

    feature: add cognito access token validation

---
+++ backend/constants/common_constants.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class UserRoles(Enum):
+    ADMIN = 'admin'
+    SUPER_ADMIN = 'super_admin'
--- backend/controller/app_controller.py
+++ backend/controller/app_controller.py
@@ -1,4 +1,4 @@
 def api_controller(app):
     from controller.user_router import user_router
 
-    app.include_router(user_router, prefix='users', tags=['Users'])
+    app.include_router(user_router, prefix='/users', tags=['Users'])
