7d7d85508360817d0d264e9ac96e52035e7d62a6
Sean Gabriel Bayron <scbayron@up.edu.ph>
2023-10-22 09:48:31 +0800

    added utility functions and provided example usage of some components

--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -1,15 +1,57 @@
+import Button from "@/components/Button";
+import {
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormError,
+} from "@/components/Form";
+import Input from "@/components/Input";
 import { useRegisterForm } from "@/hooks/useRegisterForm";
 import { FormProvider } from "react-hook-form";
 
 const Register = () => {
-  const { form, handleSubmit } = useRegisterForm();
+  const { form, submit } = useRegisterForm();
+
   return (
     <div>
       <h1>Register</h1>
       <FormProvider {...form}>
-        <input type="text" name="email" />
-        <input type="password" name="password" />
-        <button onClick={handleSubmit}>Submit</button>
+        <FormItem name="email">
+          {({ field }) => (
+            <div className="flex flex-col items-start mb-5 space-y-2">
+              <FormLabel>Email</FormLabel>
+              <Input type="email" {...field} />
+              <FormDescription className="font-subjectivity">
+                Whereas recognition of the inherent dignity
+              </FormDescription>
+              <FormError />
+            </div>
+          )}
+        </FormItem>
+
+        <FormItem name="age">
+          {({ field }) => (
+            <div className="flex flex-col items-start mb-5 space-y-2">
+              <FormLabel optional>Age</FormLabel>
+              <Input type="text" placeholder="Enter your age" {...field} />
+              <FormError />
+            </div>
+          )}
+        </FormItem>
+
+        <FormItem name="password">
+          {({ field }) => (
+            <div className="flex flex-col items-start mb-5 space-y-2">
+              <FormLabel toolTipContent={"Enter atleast 8 characters"}>
+                Password
+              </FormLabel>
+              <Input type="password" {...field} />
+              <FormError />
+            </div>
+          )}
+        </FormItem>
+
+        <Button onClick={submit}>Submit</Button>
       </FormProvider>
     </div>
   );
