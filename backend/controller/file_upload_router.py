24407cbcf92afce2e6d8beb04ca9367aa1a64fca
johnservo <johnservo.se@gmail.com>
2023-10-25 20:01:47 +0800

    feat: can now upload files to s3

--- backend/controller/file_upload_router.py
+++ backend/controller/file_upload_router.py
@@ -1,3 +1,5 @@
+import os
+
 from typing import List
 
 from aws.cognito_settings import AccessUser, get_current_user
@@ -6,6 +8,18 @@ from fastapi import APIRouter, Depends, Path
 from model.common import Message
 from model.file_uploads.file_upload import FileUploadOut
 from model.file_uploads.file_upload_constants import UploadTypes, FileUploadConstants
+
+# TO TRANSFER:
+from boto3 import client as boto3_client
+from botocore.config import Config
+
+# TODO: REMOVE ACCESS KEY IDS
+s3_client = boto3_client('s3',
+                         region_name=os.getenv('REGION', 'ap-southeast-1'),
+                         config=Config(signature_version="s3v4"),
+                         # aws_access_key_id=os.getenv('ACCESS_KEY_ID'),
+                         # aws_secret_access_key=os.getenv('SECRET_ACCESS_KEY')
+                         )
 
 file_upload_router = APIRouter()
 
@@ -20,7 +34,7 @@ file_upload_router = APIRouter()
     summary="Get presigned URL",
 )
 @file_upload_router.get(
-    '/{uploadType}',
+    '/{uploadType}/',
     response_model=FileUploadOut,
     response_model_exclude_none=True,
     response_model_exclude_unset=True,
@@ -30,6 +44,17 @@ def get_presigned_url(
     entry_id: str = Path(..., title='Event Id', alias=CommonConstants.ENTRY_ID),
     upload_type: UploadTypes = Path(..., title='Upload Type', alias=FileUploadConstants.UPLOAD_TYPE)
 ):
-    print(entry_id)
-    print(upload_type.value)
-    return { "uploadLink": "TEST", "objectKey": "TEST" }
+    PRESIGNED_URL_EXPIRATION_TIME = 30
+    S3_BUCKET = 'sparcs-events-bucket'
+    object_key = f'events/{entry_id}/{upload_type.value}'
+
+    presigned_url = s3_client.generate_presigned_url(
+        ClientMethod='put_object',
+        Params={
+            "Bucket": S3_BUCKET,
+            "Key": object_key,
+        },
+        ExpiresIn= PRESIGNED_URL_EXPIRATION_TIME
+    )
+
+    return { "uploadLink": presigned_url, "objectKey": object_key }
