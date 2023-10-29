f6cf0b90a03e2b478602b784676e26cf6134ec40
Sean Gabriel Bayron <scbayron@up.edu.ph>
2023-10-29 21:48:13 +0800

    style(routes): fixed route

--- frontend/src/routes/routes.ts
+++ frontend/src/routes/routes.ts
@@ -30,7 +30,7 @@ export const routes = createBrowserRouter(
       element: AdminLoginPage(),
     },
     {
-      path: "admin/events",
+      path: "/admin/events",
       element: AdminPage(),
       children: [
         {
