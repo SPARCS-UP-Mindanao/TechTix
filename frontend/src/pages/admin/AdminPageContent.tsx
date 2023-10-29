40426a89e92a668b7f850bdf571b14a7d1c8cfa6
Sean Gabriel Bayron <scbayron@up.edu.ph>
2023-10-29 21:45:53 +0800

    style: removed get event

--- frontend/src/pages/admin/AdminPageContent.tsx
+++ frontend/src/pages/admin/AdminPageContent.tsx
@@ -2,12 +2,11 @@ import { Outlet as AdminPageRoute, Navigate } from "react-router-dom";
 import AdminPageHeader from "./AdminPageHeader";
 import { useIsAuthenticated } from "react-auth-kit";
 import { useApi } from "@/hooks/useApi";
-import { getAllEvents, getEvent } from "@/api/events";
+import { getAllEvents } from "@/api/events";
 
 const AdminPageContent = () => {
   const isAuthenticated = useIsAuthenticated();
   const { data: allEvents } = useApi(getAllEvents());
-  const { data: event } = useApi(getEvent("01HCXXDEAHNG0S57WG2MHVNRYV"));
 
   if (!isAuthenticated()) {
     return <Navigate to="/admin/login" />;
@@ -20,8 +19,6 @@ const AdminPageContent = () => {
       {allEvents?.map((event) => (
         <p>{event.name}</p>
       ))}
-
-      <h1 className="text-secondary">{event?.name}</h1>
       <AdminPageRoute />
     </div>
   );
