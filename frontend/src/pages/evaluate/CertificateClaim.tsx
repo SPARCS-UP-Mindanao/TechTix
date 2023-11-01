02951fbb520a097e130d60dda888882175274207
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2023-11-01 14:12:52 +0800

    feat: modified claimcaertificate to match updated ui

--- frontend/src/pages/evaluate/CertificateClaim.tsx
+++ frontend/src/pages/evaluate/CertificateClaim.tsx
@@ -1,46 +1,36 @@
-import Avatar from "@/components/Avatar";
-import Button from "@/components/Button";
-import Icon from "@/components/Icon";
-import image_placeholder from "./image_placeholder/placeholder.png";
+import sparcs_logo_white from '@/assets/logos/sparcs_logo_white.png';
+import Avatar from '@/components/Avatar';
+import Button from '@/components/Button';
+import Icon from '@/components/Icon';
+import image_placeholder from './image_placeholder/placeholder.png';
 
 const CertificateClaim = () => {
   return (
     <>
       <div className="flex flex-col items-center">
         <div className="h-12 w-12">
-          <Avatar src="https://github.com/shadcn.png" fallback="CN" />
+          <Avatar src={sparcs_logo_white} fallback="CN" />
         </div>
-        <p className="mt-2 text-2xl font-bold">Here's your Certificate!</p>
-        <div className="mt-6 sm:h-72 w-[91vw]">
-          <img
-            src={image_placeholder}
-            alt="image_placeholder"
-            className="rounded-2xl object-center object-cover w-full h-full"
-          />
-        </div>
-        <div className="flex flex-col mt-4 w-[91vw]">
-          <Button
-            variant="default"
-            className="mt-2.5 focus:bg-[#56B32F] bg-[#56B32F] text-[#F6F6F6]"
-          >
-            <Icon name="DownloadSimple" className="mr-2 h-4 w-4" />
-            Download Image
-          </Button>
-          <Button
-            variant="default"
-            className="mt-2.5 focus:bg-[#56B32F] bg-[#56B32F] text-[#F6F6F6]"
-          >
-            <Icon name="DownloadSimple" className="mr-2 h-4 w-4" />
-            Download PDF
-          </Button>
-          <Button
-            variant="default"
-            className="mt-2.5 focus:bg-[#475FE9] bg-[#475FE9] text-[#F6F6F6]"
-          >
-            <Icon name="ShareNetwork" className="mr-2 h-4 w-4" />
-            Share
-          </Button>
-          <Icon name="download-simple" className="mr-2 h-4 w-4" />
+        <div className="mt-12">
+          <p className="text-2xl font-bold font-subjectivity leading-6 text-neutral-50 text-center">Here's your Certificate!</p>
+          <div className="my-5 sm:h-72 w-[91vw]">
+            <img src={image_placeholder} alt="image_placeholder" className="rounded-2xl object-center object-cover w-full h-full" />
+          </div>
+          <div className="flex flex-col pt-3 w-[91vw] space-y-3">
+            <Button variant="default" className="py-3 px-6 font-raleway bg-gradient-to-r from-[#D95229] to-[#F4805D] text-[#F6F6F6]">
+              <Icon name="DownloadSimple" className="mr-2 h-4 w-4" />
+              Download Image
+            </Button>
+            <Button variant="default" className="py-3 px-6 font-raleway bg-gradient-to-r from-[#D95229] to-[#F4805D] text-[#F6F6F6]">
+              <Icon name="DownloadSimple" className="mr-2 h-4 w-4" />
+              Download PDF
+            </Button>
+            <Button variant="default" className="py-3 px-6 font-raleway bg-gradient-to-r from-[#4F65E3] to-[#F43F79] text-[#F6F6F6]">
+              <Icon name="ShareNetwork" className="mr-2 h-4 w-4" />
+              Share
+            </Button>
+            <Icon name="download-simple" className="mr-2 h-4 w-4" />
+          </div>
         </div>
       </div>
     </>
--- frontend/src/pages/evaluate/EventInformation.tsx
+++ frontend/src/pages/evaluate/EventInformation.tsx
@@ -1,40 +1,35 @@
-import Avatar from "@/components/Avatar";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  FormDescription,
-  FormItem,
-  FormLabel,
-  FormError,
-} from "@/components/Form";
-import Input from "@/components/Input";
-import { FormProvider } from "react-hook-form";
-import Button from "@/components/Button";
-import Icon from "@/components/Icon";
-import image_placeholder from "./image_placeholder/sparcs-image-placeholder.png";
-import sparcs_logo_white from "../../assets/logos/sparcs_logo_white.png"
+import { useForm } from 'react-hook-form';
+import { FormProvider } from 'react-hook-form';
+import { z } from 'zod';
+import Avatar from '@/components/Avatar';
+import Button from '@/components/Button';
+import { FormDescription, FormItem, FormLabel, FormError } from '@/components/Form';
+import Icon from '@/components/Icon';
+import Input from '@/components/Input';
+import sparcs_logo_white from '../../assets/logos/sparcs_logo_white.png';
+import image_placeholder from './image_placeholder/sparcs-image-placeholder.png';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const ClaimCertificateSchema = z.object({
   email: z.string().email({
-    message: "Please enter a valid email address",
-  }),
+    message: 'Please enter a valid email address'
+  })
 });
 
 // TO REMOVE
-const logo_placeholder = [
-  "1UNEuxhoCBbFXkEnmWfpwaBykjLTkOMDE",
-  "1NXwPcnLz_FiLeilirIOly-o5Pa_lFqh1",
-  "15ouLpbrCkm-PNbr2JJiz-QNruKj2zURc",
-  "1J1H5NsTH37NjSM768dLZCVjLBMSIfNcv",
-];
+// const logo_placeholder = [
+//   "1UNEuxhoCBbFXkEnmWfpwaBykjLTkOMDE",
+//   "1NXwPcnLz_FiLeilirIOly-o5Pa_lFqh1",
+//   "15ouLpbrCkm-PNbr2JJiz-QNruKj2zURc",
+//   "1J1H5NsTH37NjSM768dLZCVjLBMSIfNcv",
+// ];
 
 const EventInformation = () => {
   const form = useForm<z.infer<typeof ClaimCertificateSchema>>({
     resolver: zodResolver(ClaimCertificateSchema),
     defaultValues: {
-      email: "",
-    },
+      email: ''
+    }
   });
 
   const submit = form.handleSubmit((data) => {
@@ -48,18 +43,14 @@ const EventInformation = () => {
           <Avatar src={sparcs_logo_white} fallback="CN" />
         </div>
         <div className="mt-6 sm:h-72 w-[91vw] drop-shadow-xl">
-          <img
-            src={image_placeholder}
-            alt="image_placeholder"
-            className="rounded-2xl object-center object-cover w-full h-full"
-          />
+          <img src={image_placeholder} alt="image_placeholder" className="rounded-2xl object-center object-cover w-full h-full" />
         </div>
         <div className="mt-6 w-[91vw]">
           <p className="text-xl font-subjectivity font-bold text-left leading-6">UP Mindanao SPARCS Application A.Y 2023 - 2024</p>
           <div className="w-full mt-3.5 space-y-1.5 items-start">
             <div className="flex">
               <Icon name="Clock" weight="light" className="w-6 h-6" />
-              <span className="text-sm font-raleway font-medium text-left leading-5 ml-1">November 11, 2023  |  12:30 – 5:00 PM GMT+8</span>
+              <span className="text-sm font-raleway font-medium text-left leading-5 ml-1">November 11, 2023 | 12:30 – 5:00 PM GMT+8</span>
             </div>
             <div className="flex">
               <Icon name="MapPin" weight="light" className="w-6 h-6" />
@@ -70,32 +61,30 @@ const EventInformation = () => {
           <div>
             <p className="text-left font-raleway font-semibold text-lg leading-5 tracking-tight mb-6">Claim your certificate by evaluating the event</p>
             <FormProvider {...form}>
-                <FormItem name="email">
-                  {({ field }) => (
-                    <div className="flex flex-col items-start space-y-2">
-                      <FormLabel className="font-raleway text-neutral-50 font-medium leading-5 tracking-tight">
-                        Enter your e-mail
-                      </FormLabel>
-                      <Input
-                        type="email"
-                        {...field}
-                        className="text-neutral-300 rounded-2xl py-3 px-6 my-3 bg-neutral-900 tracking-tighter leading-5"
-                        placeholder="Email"
-                      />
-                      <FormDescription className="text-left font-raleway font-medium text-sm leading-4 tracking-tighter text-neutral-300">
-                        *Please enter the email address you used when registering for the event 
-                      </FormDescription>
-                      <FormError />
-                    </div>
-                  )}
-                </FormItem>
-                <Button
-                  onClick={submit}
-                  className="py-3 px-12 rounded-2xl font-bold font-raleway leading text-white bg-gradient-to-r from-[#4F65E3] to-[#F43F79]"
-                >
-                  Evaluate
-                </Button>
-              </FormProvider>
+              <FormItem name="email">
+                {({ field }) => (
+                  <div className="flex flex-col items-start space-y-2">
+                    <FormLabel className="font-raleway text-neutral-50 font-medium leading-5 tracking-tight">Enter your e-mail</FormLabel>
+                    <Input
+                      type="email"
+                      {...field}
+                      className="text-neutral-300 rounded-2xl py-3 px-6 my-3 bg-neutral-900 tracking-tighter leading-5"
+                      placeholder="Email"
+                    />
+                    <FormDescription className="text-left font-raleway font-medium text-sm leading-4 tracking-tighter text-neutral-300">
+                      *Please enter the email address you used when registering for the event
+                    </FormDescription>
+                    <FormError />
+                  </div>
+                )}
+              </FormItem>
+              <Button
+                onClick={submit}
+                className="py-3 px-12 rounded-2xl font-bold font-raleway leading text-white bg-gradient-to-r from-[#4F65E3] to-[#F43F79] mt-12"
+              >
+                Evaluate
+              </Button>
+            </FormProvider>
           </div>
         </div>
         {/* <p className="mt-7 text-xl font-bold">
