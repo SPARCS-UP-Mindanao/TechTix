5eaeaae3c18e42089d3ace7519f30ac2d5e817c0
Sean Gabriel Bayron <scbayron@up.edu.ph>
2023-12-05 23:46:10 +0800

    feat: updated login page and created hook for forgot password feature

--- frontend/src/pages/admin/login/LoginForm.tsx
+++ frontend/src/pages/admin/login/LoginForm.tsx
@@ -1,13 +1,113 @@
+import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useIsAuthenticated } from 'react-auth-kit';
 import { FormProvider } from 'react-hook-form';
 import Button from '@/components/Button';
 import { FormItem, FormLabel, FormError } from '@/components/Form';
 import Input from '@/components/Input';
+import Modal from '@/components/Modal';
+import { isEmpty } from '@/utils/functions';
 import { useAdminLoginForm } from '@/hooks/useAdminLoginForm';
+import { useForgetPassword } from '@/hooks/useForgetPassword';
+
+export type ResetModalSteps = 'email' | 'submit';
+
+const ResetPasswordModal = () => {
+  const [step, setStep] = useState<ResetModalSteps>('email');
+  const { resetPasswordForm, sendCodeToEmail, resetPassword, showModal, toggleModal, isEmailSubmitting, isFormSubmitting, isFormValid, sendCodeDisabled } =
+    useForgetPassword(setStep);
+
+  const ModalFooter = () => {
+    return (
+      <div className="w-full flex justify-center">
+        {step === 'email' && (
+          <Button onClick={async () => await sendCodeToEmail()} disabled={sendCodeDisabled} loading={isEmailSubmitting}>
+            Send code
+          </Button>
+        )}
+        {step === 'submit' && (
+          <Button onClick={resetPassword} disabled={!isFormValid} loading={isFormSubmitting}>
+            Reset Password
+          </Button>
+        )}
+      </div>
+    );
+  };
+
+  const getModalDescription = () => {
+    if (step === 'email') {
+      return 'Enter your email and we will send you a code to reset your password.';
+    }
+    if (step === 'submit') {
+      return 'Enter the code sent to your email and your new password.';
+    }
+  };
+
+  return (
+    <Modal
+      modalTitle="Reset Password"
+      modalDescription={getModalDescription()}
+      visible={showModal}
+      onOpenChange={toggleModal}
+      modalFooter={<ModalFooter />}
+      trigger={
+        <Button variant="link" className="text-foreground">
+          Forgot Password?
+        </Button>
+      }
+    >
+      <FormProvider {...resetPasswordForm}>
+        <main>
+          {step === 'email' && (
+            <FormItem name="email">
+              {({ field }) => (
+                <div className="space-y-2">
+                  <FormLabel>Email</FormLabel>
+                  <Input type="email" {...field} />
+                  <FormError />
+                </div>
+              )}
+            </FormItem>
+          )}
+          {step === 'submit' && (
+            <>
+              <FormItem name="confirmationCode">
+                {({ field }) => (
+                  <div className="space-y-2">
+                    <FormLabel>Confirmation code</FormLabel>
+                    <Input type="text" {...field} />
+                    <FormError />
+                  </div>
+                )}
+              </FormItem>
+              <FormItem name="password">
+                {({ field }) => (
+                  <div className="space-y-2">
+                    <FormLabel>Password</FormLabel>
+                    <Input type="password" {...field} />
+                    <FormError />
+                  </div>
+                )}
+              </FormItem>
+              <FormItem name="confirmPassword">
+                {({ field }) => (
+                  <div className="space-y-2">
+                    <FormLabel>Confirm Password</FormLabel>
+                    <Input type="password" {...field} />
+                    <FormError />
+                  </div>
+                )}
+              </FormItem>
+            </>
+          )}
+        </main>
+      </FormProvider>
+    </Modal>
+  );
+};
 
 const LoginForm = () => {
-  const { form, submit } = useAdminLoginForm();
+  const { form, submit, isSubmitting } = useAdminLoginForm();
   const isAuthenticated = useIsAuthenticated();
 
   if (isAuthenticated()) {
@@ -15,9 +115,7 @@ const LoginForm = () => {
   }
 
   return (
-    <section className="flex flex-col space-y-4 w-full max-w-2xl">
-      <h1 className="text-center">Admin login</h1>
-
+    <>
       <FormProvider {...form}>
         <FormItem name="email">
           {({ field }) => (
@@ -39,13 +137,14 @@ const LoginForm = () => {
           )}
         </FormItem>
 
-        <div className="w-full flex justify-center">
-          <Button onClick={submit} className="w-full min-w-min max-w-[40%]">
+        <div className="w-full flex justify-between">
+          <ResetPasswordModal />
+          <Button onClick={submit} className="w-full min-w-min max-w-[20%]" loading={isSubmitting}>
             Submit
           </Button>
         </div>
       </FormProvider>
-    </section>
+    </>
   );
 };
 
