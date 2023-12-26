ea44a112d2c2ff3aed53f43a691b0b7da14a4a50
Sean Gabriel Bayron <scbayron@up.edu.ph>
2023-12-26 11:45:27 +0800

    feat: added assets for new techtix logo

---
+++ frontend/src/assets/techtix.ts
@@ -0,0 +1,7 @@
+import logo_72 from '../../public/icon-72x72.png';
+import word_72_dark from '../../public/techtix-72-dark.png';
+import word_72 from '../../public/techtix-72.png';
+
+export const TECHTIX_72 = logo_72;
+export const TECHTIX_WORD_72 = word_72;
+export const TECHTIX_WORD_72_DARK = word_72_dark;
