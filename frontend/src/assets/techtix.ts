45e3867860acff262357916aaea7972330dfdcbf
Sean Gabriel Bayron <scbayron@up.edu.ph>
2023-12-27 15:49:03 +0800

    feat: imported assets in one file

--- frontend/src/assets/techtix.ts
+++ frontend/src/assets/techtix.ts
@@ -1,6 +1,6 @@
-import logo_72 from '../../public/icon-72x72.png';
-import word_72_dark from '../../public/techtix-72-dark.png';
-import word_72 from '../../public/techtix-72.png';
+import logo_72 from '/icon-72x72.png';
+import word_72_dark from '/techtix-72-dark.png';
+import word_72 from '/techtix-72.png';
 
 export const TECHTIX_72 = logo_72;
 export const TECHTIX_WORD_72 = word_72;
