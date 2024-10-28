462e2f3b2b90ada55c651107731d7e666ee7383c
44mira <legolaslada@gmail.com>
2024-10-28 23:48:51 +0800

    feat: get_csv controller signature

--- backend/controller/preregistration_router.py
+++ backend/controller/preregistration_router.py
@@ -10,6 +10,7 @@ from model.preregistrations.preregistration import (
     PreRegistrationOut,
     PreRegistrationPatch,
 )
+from model.file_uploads.file_upload import FileDownloadOut
 from pydantic import EmailStr
 from usecase.preregistration_usecase import PreRegistrationUsecase
 
@@ -17,23 +18,23 @@ preregistration_router = APIRouter()
 
 
 @preregistration_router.get(
-    '',
+    "",
     response_model=List[PreRegistrationOut],
     responses={
-        404: {'model': Message, 'description': 'Pre-registration not found'},
-        500: {'model': Message, 'description': 'Internal server error'},
+        404: {"model": Message, "description": "Pre-registration not found"},
+        500: {"model": Message, "description": "Internal server error"},
     },
-    summary='Get pre-registrations',
+    summary="Get pre-registrations",
 )
 @preregistration_router.get(
-    '/',
+    "/",
     response_model=List[PreRegistrationOut],
     response_model_exclude_none=True,
     response_model_exclude_unset=True,
     include_in_schema=False,
 )
 def get_preregistrations(
-    event_id: str = Query(None, title='Event Id', alias=CommonConstants.EVENT_ID),
+    event_id: str = Query(None, title="Event Id", alias=CommonConstants.EVENT_ID),
 ):
     """Get a list of pre-registration entries
 
@@ -49,24 +50,26 @@ def get_preregistrations(
 
 
 @preregistration_router.get(
-    '/{entryId}',
+    "/{entryId}",
     response_model=PreRegistrationOut,
     responses={
-        404: {'model': Message, 'description': 'Pre-registration not found'},
-        500: {'model': Message, 'description': 'Internal server error'},
+        404: {"model": Message, "description": "Pre-registration not found"},
+        500: {"model": Message, "description": "Internal server error"},
     },
-    summary='Get pre-registration',
+    summary="Get pre-registration",
 )
 @preregistration_router.get(
-    '/{entryId}/',
+    "/{entryId}/",
     response_model=PreRegistrationOut,
     response_model_exclude_none=True,
     response_model_exclude_unset=True,
     include_in_schema=False,
 )
 def get_preregistration(
-    entry_id: str = Path(..., title='Pre-registration Id', alias=CommonConstants.ENTRY_ID),
-    event_id: str = Query(..., title='Event Id', alias=CommonConstants.EVENT_ID),
+    entry_id: str = Path(
+        ..., title="Pre-registration Id", alias=CommonConstants.ENTRY_ID
+    ),
+    event_id: str = Query(..., title="Event Id", alias=CommonConstants.EVENT_ID),
 ):
     """Get a specific pre-registration entry by its ID.
 
@@ -81,28 +84,30 @@ def get_preregistration(
 
     """
     preregistrations_uc = PreRegistrationUsecase()
-    return preregistrations_uc.get_preregistration(event_id=event_id, preregistration_id=entry_id)
+    return preregistrations_uc.get_preregistration(
+        event_id=event_id, preregistration_id=entry_id
+    )
 
 
 @preregistration_router.get(
-    '/{email}/email',
+    "/{email}/email",
     response_model=PreRegistrationOut,
     responses={
-        404: {'model': Message, 'description': 'Pre-registration not found'},
-        500: {'model': Message, 'description': 'Internal server error'},
+        404: {"model": Message, "description": "Pre-registration not found"},
+        500: {"model": Message, "description": "Internal server error"},
     },
-    summary='Get pre-registration',
+    summary="Get pre-registration",
 )
 @preregistration_router.get(
-    '/{email}/email/',
+    "/{email}/email/",
     response_model=PreRegistrationOut,
     response_model_exclude_none=True,
     response_model_exclude_unset=True,
     include_in_schema=False,
 )
 def get_preregistration_by_email(
-    email: EmailStr = Path(..., title='Email'),
-    event_id: str = Query(..., title='Event Id', alias=CommonConstants.EVENT_ID),
+    email: EmailStr = Path(..., title="Email"),
+    event_id: str = Query(..., title="Event Id", alias=CommonConstants.EVENT_ID),
 ):
     """Get a specific pre-registration email used.
 
@@ -114,24 +119,26 @@ def get_preregistration_by_email(
 
     """
     preregistrations_uc = PreRegistrationUsecase()
-    return preregistrations_uc.get_preregistration_by_email(event_id=event_id, email=email)
+    return preregistrations_uc.get_preregistration_by_email(
+        event_id=event_id, email=email
+    )
 
 
 @preregistration_router.post(
-    '',
+    "",
     response_model=PreRegistrationOut,
     responses={
-        400: {'model': Message, 'description': 'Invalid input'},
+        400: {"model": Message, "description": "Invalid input"},
         409: {
-            'model': Message,
-            'description': 'Pre-registration with email example@example.com already exists',
+            "model": Message,
+            "description": "Pre-registration with email example@example.com already exists",
         },
-        500: {'model': Message, 'description': 'Internal server error'},
+        500: {"model": Message, "description": "Internal server error"},
     },
-    summary='Create pre-registration',
+    summary="Create pre-registration",
 )
 @preregistration_router.post(
-    '/',
+    "/",
     response_model=PreRegistrationOut,
     response_model_exclude_none=True,
     response_model_exclude_unset=True,
@@ -154,17 +161,17 @@ def create_preregistration(
 
 
 @preregistration_router.put(
-    '/{entryId}',
+    "/{entryId}",
     response_model=PreRegistrationOut,
     responses={
-        400: {'model': Message, 'description': 'Bad request'},
-        404: {'model': Message, 'description': 'Pre-registration not found'},
-        500: {'model': Message, 'description': 'Internal server error'},
+        400: {"model": Message, "description": "Bad request"},
+        404: {"model": Message, "description": "Pre-registration not found"},
+        500: {"model": Message, "description": "Internal server error"},
     },
-    summary='Update pre-registration',
+    summary="Update pre-registration",
 )
 @preregistration_router.put(
-    '/{entryId}/',
+    "/{entryId}/",
     response_model=PreRegistrationOut,
     response_model_exclude_none=True,
     response_model_exclude_unset=True,
@@ -172,8 +179,10 @@ def create_preregistration(
 )
 def update_preregistration(
     preregistration: PreRegistrationPatch,
-    entry_id: str = Path(..., title='Pre-registration Id', alias=CommonConstants.ENTRY_ID),
-    event_id: str = Query(..., title='Event Id', alias=CommonConstants.EVENT_ID),
+    entry_id: str = Path(
+        ..., title="Pre-registration Id", alias=CommonConstants.ENTRY_ID
+    ),
+    event_id: str = Query(..., title="Event Id", alias=CommonConstants.EVENT_ID),
     current_user: AccessUser = Depends(get_current_user),
 ):
     """Update an existing pre-registration entry.
@@ -197,26 +206,30 @@ def update_preregistration(
     _ = current_user
     preregistrations_uc = PreRegistrationUsecase()
     return preregistrations_uc.update_preregistration(
-        event_id=event_id, preregistration_id=entry_id, preregistration_in=preregistration
+        event_id=event_id,
+        preregistration_id=entry_id,
+        preregistration_in=preregistration,
     )
 
 
 @preregistration_router.delete(
-    '/{entryId}',
+    "/{entryId}",
     status_code=HTTPStatus.NO_CONTENT,
     responses={
-        204: {'description': 'Pre-registration deletion success', 'content': None},
+        204: {"description": "Pre-registration deletion success", "content": None},
     },
-    summary='Delete pre-registration',
+    summary="Delete pre-registration",
 )
 @preregistration_router.delete(
-    '{entryId}/',
+    "{entryId}/",
     status_code=HTTPStatus.NO_CONTENT,
     include_in_schema=False,
 )
 def delete_preregistration(
-    entry_id: str = Path(..., title='Pre-registration Id', alias=CommonConstants.ENTRY_ID),
-    event_id: str = Query(..., title='Event Id', alias=CommonConstants.EVENT_ID),
+    entry_id: str = Path(
+        ..., title="Pre-registration Id", alias=CommonConstants.ENTRY_ID
+    ),
+    event_id: str = Query(..., title="Event Id", alias=CommonConstants.EVENT_ID),
     current_user: AccessUser = Depends(get_current_user),
 ):
     """Delete a specific registration entry by its ID.
@@ -236,4 +249,40 @@ def delete_preregistration(
     """
     _ = current_user
     preregistrations_uc = PreRegistrationUsecase()
-    return preregistrations_uc.delete_preregistration(preregistration_id=entry_id, event_id=event_id)
+    return preregistrations_uc.delete_preregistration(
+        preregistration_id=entry_id, event_id=event_id
+    )
+
+
+@preregistration_router.get(
+    "/{entryId}/csv_download",
+    response_model=FileDownloadOut,
+    responses={
+        404: {"model": Message, "description": "Pre-registration not found"},
+        500: {"model": Message, "description": "Internal server error"},
+    },
+    summary="Get CSV for pre-registration",
+)
+@preregistration_router.get(
+    "/{eventId}/csv_download/",
+    response_model=FileDownloadOut,
+    response_model_exclude_none=True,
+    response_model_exclude_unset=True,
+    include_in_schema=False,
+)
+def get_preregistration_csv(
+    event_id: str = Path(..., title="Event Id", alias=CommonConstants.EVENT_ID),
+):
+    """Get the CSV for a specific event
+
+    :param entry_id: The pre-registration ID.
+    :type entry_id: str
+
+    :param event_id: The event ID.
+    :type event_id: str
+
+    :return: The csv for the corresponding event
+    :rtype: FileDownloadOut
+
+    """
+    pass
