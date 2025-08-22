9f77d9c11cd743ef4308ce52d3ca773a558cd438
seangaaab <seangabbayron@gmail.com>
2025-08-23 02:38:22 +0800

    feat: update env example file and updated route and amplify config

--- frontend/amplify/backend.ts
+++ frontend/amplify/backend.ts
@@ -4,16 +4,14 @@ const backend = defineBackend({});
 backend.addOutput({
   auth: {
     aws_region: 'ap-southeast-1',
-    // user_pool_id: process.env.COGNITO_POOL_ID,
-    // user_pool_client_id: process.env.COGNITO_POOL_CLIENT_ID,
     user_pool_id: process.env.COGNITO_POOL_ID,
     user_pool_client_id: process.env.COGNITO_POOL_CLIENT_ID,
     oauth: {
       identity_providers: ['GOOGLE'],
       domain: process.env.COGNITO_DOMAIN_URL!,
       scopes: ['email', 'openid', 'phone', 'profile', 'aws.cognito.signin.user.admin'],
-      redirect_sign_in_uri: [process.env.COGNITO_DOMAIN_URL!, `${process.env.COGNITO_DOMAIN_URL}/`],
-      redirect_sign_out_uri: [process.env.COGNITO_DOMAIN_URL!, `${process.env.COGNITO_DOMAIN_URL}/`],
+      redirect_sign_in_uri: [`${process.env.FRONTEND_BASE_URL}/signin/callback`],
+      redirect_sign_out_uri: [],
       response_type: 'token'
     }
   }
