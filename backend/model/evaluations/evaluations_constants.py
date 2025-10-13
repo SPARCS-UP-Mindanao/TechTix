7c66f3cacc532579e839112f5819e3c67ac64022
jl <orionjl1106@gmail.com>
2025-10-13 15:06:22 +0800

    fix: remove comments and fix typo

--- backend/model/evaluations/evaluations_constants.py
+++ backend/model/evaluations/evaluations_constants.py
@@ -15,45 +15,34 @@ class EvaluationQuestionType(str, Enum):
 
 
 class PyconEvaluationQuestionType(str, Enum):
-    HOW_WOULD_YOU_RATE_PYCON_DAVAO_OVERALL = 'how_would_you_rate_pycon_davao_overall'  # (1-5)
-    WhAT_WAS_THE_HIGHLIGHT_OF_THE_EVENT_FOR_YOU = 'what_was_the_highlight_of_the_event_for_you'  # Text
+    HOW_WOULD_YOU_RATE_PYCON_DAVAO_OVERALL = 'how_would_you_rate_pycon_davao_overall'
+    WHAT_WAS_THE_HIGHLIGHT_OF_THE_EVENT_FOR_YOU = 'what_was_the_highlight_of_the_event_for_you'
     IS_THERE_ANYTHING_SPECIFIC_YOU_WOULD_LIKE_TO_COMMEND_ABOUT_THE_EVENT = (
-        'is_there_anything_specific_you_would_like_to_commend_about_the_event'  # Text
+        'is_there_anything_specific_you_would_like_to_commend_about_the_event'
     )
     HOW_SATISFIED_WERE_YOU_WITH_THE_CONTENT_AND_PRESENTATIONS = (
-        'how_satisfied_were_you_with_the_content_and_presentations'  # (1-5)
+        'how_satisfied_were_you_with_the_content_and_presentations'
     )
-    WHICH_SESSIONS_OR_TOPICS_DID_YOU_FIND_MOST_VALUABLE = 'which_sessions_or_topics_did_you_find_most_valuable'  # Text
-    WERE_THERE_ANY_TOPICS_OR_SESSIONS_YOU_FELT_WERE_MISSING = (
-        'were_there_any_topics_or_sessions_you_felt_were_missing'  # Text
-    )
-    HAVE_YOU_ATTENDED_PYTHON_EVENTS_BEFORE = 'have_you_attended_python_events_before_if_so_what_kind_of_community_support_did_you_find_helpful_in_previous_events'  # Text
-    HOW_LIKELY_ARE_YOU_TO_RECOMMEND_THIS_EVENT = 'how_likely_are_you_to_recommend_this_event_to_other_python_developers_or_enthusiasts_based_on_the_community_support_provided'  # (1-5)
-    # Organization and Logistics
-    HOW_WOULD_YOU_RATE_EVENT_ORGANIZATION_AND_LOGISTICS = 'how_would_you_rate_event_organization_and_logistics'  # (1-5)
-    WERE_THE_EVENT_SCHEDULE_AND_TIMING_CONVENIENT_FOR_YOU = (
-        'were_the_event_schedule_and_timing_convenient_for_you'  # (1-5)
-    )
-    WERE_THE_VENUE_FACILITIES_ADEQUATE = 'were_the_venue_facilities_adequate'  # (1-5)
+    WHICH_SESSIONS_OR_TOPICS_DID_YOU_FIND_MOST_VALUABLE = 'which_sessions_or_topics_did_you_find_most_valuable'
+    WERE_THERE_ANY_TOPICS_OR_SESSIONS_YOU_FELT_WERE_MISSING = 'were_there_any_topics_or_sessions_you_felt_were_missing'
+    HAVE_YOU_ATTENDED_PYTHON_EVENTS_BEFORE = 'have_you_attended_python_events_before_if_so_what_kind_of_community_support_did_you_find_helpful_in_previous_events'
+    HOW_LIKELY_ARE_YOU_TO_RECOMMEND_THIS_EVENT = 'how_likely_are_you_to_recommend_this_event_to_other_python_developers_or_enthusiasts_based_on_the_community_support_provided'
+    HOW_WOULD_YOU_RATE_EVENT_ORGANIZATION_AND_LOGISTICS = 'how_would_you_rate_event_organization_and_logistics'
+    WERE_THE_EVENT_SCHEDULE_AND_TIMING_CONVENIENT_FOR_YOU = 'were_the_event_schedule_and_timing_convenient_for_you'
+    WERE_THE_VENUE_FACILITIES_ADEQUATE = 'were_the_venue_facilities_adequate'
     DID_YOU_ENCOUNTER_ANY_PROBLEMS_WITH_THE_ORGANIZATION_AND_LOGISTICS = (
-        'did_you_encounter_any_problems_with_the_organization_and_logistics'  # Text
+        'did_you_encounter_any_problems_with_the_organization_and_logistics'
     )
-    # Networking and Interaction
-    DID_YOU_FIND_OPPORTUNITIES_FOR_NETWORKING_VALUABLE = 'did_you_find_opportunities_for_networking_valuable'  # (1-5)
+    DID_YOU_FIND_OPPORTUNITIES_FOR_NETWORKING_VALUABLE = 'did_you_find_opportunities_for_networking_valuable'
     HOW_WOULD_YOU_RATE_THE_NETWORKING_OPPORTUNITIES_PROVIDED = (
-        'how_would_you_rate_the_networking_opportunities_provided'  # (1-5)
+        'how_would_you_rate_the_networking_opportunities_provided'
     )
     WHAT_COULD_HAVE_BEEN_IMPROVED_RELATED_TO_THE_NETWORKING = (
-        'what_could_have_been_improved_related_to_the_networking_and_interaction_aspects_of_the_event'  # Text
+        'what_could_have_been_improved_related_to_the_networking_and_interaction_aspects_of_the_event'
     )
-    # Suggestions and Improvements
-    SUGGESTIONS_FOR_IMPROVING_FUTURE_PYCON_EVENTS = (
-        'what_suggestions_do_you_have_for_improving_future_pycon_events'  # Text
-    )
-    ADDITIONAL_COMMENTS = 'is_there_anything_else_you_would_like_to_share_about_your_experience_at_the_event'  # Text
-    LIKELIHOOD_OF_ATTENDING_FUTURE_PYCON_DAVAO_EVENTS = (
-        'how_likely_are_you_to_attend_future_pycon_davao_events'  # (1-5)
-    )
+    SUGGESTIONS_FOR_IMPROVING_FUTURE_PYCON_EVENTS = 'what_suggestions_do_you_have_for_improving_future_pycon_events'
+    ADDITIONAL_COMMENTS = 'is_there_anything_else_you_would_like_to_share_about_your_experience_at_the_event'
+    LIKELIHOOD_OF_ATTENDING_FUTURE_PYCON_DAVAO_EVENTS = 'how_likely_are_you_to_attend_future_pycon_davao_events'
 
 
 class QuestionType(str, Enum):
